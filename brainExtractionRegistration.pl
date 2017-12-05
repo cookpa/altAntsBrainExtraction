e7ec6991d3ed4fb803e20675891c7780e56d8a7c
Philip A Cook <cookpa@mail.med.upenn.edu>
2017-12-05 14:52:24 -0500

    WIP: Translation parameters

--- brainExtractionRegistration.pl
+++ brainExtractionRegistration.pl
@@ -185,8 +185,9 @@ else {
 
 
 # Get initial moving transform to template
-my $antsAffInitRes = "4 4 4";
+
 # in mm
+my $antsAffInitRes = "4 4 4";
 my $antsAffInitSmooth = "3";
 
 my $downsampleTemplate = "${tmpDir}/templateDownsample.nii.gz";
@@ -205,7 +206,7 @@ my $initialAffine = "${tmpDir}/initialAffine.mat";
 # searchFactor = step size
 # arcFraction = fraction of arc to search 1 = +/- 180 degrees, 0.5 = +/- 90 degrees
 #
-my $antsAICmd = "${antsPath}antsAI -d 3 -v 1 -m Mattes[$downsampleTemplate, $downsampleHeadImage, 32, Regular, 0.2] -t Rigid[0.1] -s [18, 0.1] -c 10 -g [40, 40x40x40] -o $initialAffine";
+my $antsAICmd = "${antsPath}antsAI -d 3 -v 1 -m Mattes[$downsampleTemplate, $downsampleHeadImage, 32, Regular, 0.2] -t Similarity[0.1] -s [18, 0.2] -c 10 -g [40x40x40, 40] -o $initialAffine";
 
 if ($useTemplateRegMask) {
 
